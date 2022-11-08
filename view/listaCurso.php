90ba269544327061bf225e8d3096e77987b2dcde
LyraMaker <gabriel.lyrat@gmail.com>
2022-11-08 11:48:10 -0300

    Gerado o botão de editar e excluir

--- view/listaCurso.php
+++ view/listaCurso.php
@@ -7,8 +7,12 @@
     <a href="./novo-curso" class="btn btn-primary mb-4">Novo Curso</a>
     <ul class="list-group">
         <?php foreach ($cursos as $curso): ?>
-            <li class="list-group-item">
-                <?= $curso->getDescricao(); ?>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span><?= $curso->getDescricao(); ?></span>
+                <span>
+                <a class="btn btn-primary mr-1 text-light">Editar</a>
+                <a class="btn btn-danger mr-1 text-light">Excluir</a>
+                </span>
             </li>
         <?php endforeach; ?>
     </ul>
